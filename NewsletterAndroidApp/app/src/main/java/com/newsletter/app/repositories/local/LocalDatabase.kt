49d325a8ab196869888ea526693bca5c4a60f183
chaymae4 <71353669+chaymae4@users.noreply.github.com>
2020-11-22 15:55:04 +0100

    Update LocalDatabase.kt

--- NewsletterAndroidApp/app/src/main/java/com/newsletter/app/repositories/local/LocalDatabase.kt
+++ NewsletterAndroidApp/app/src/main/java/com/newsletter/app/repositories/local/LocalDatabase.kt
@@ -8,4 +8,5 @@ import com.newsletter.app.dataModels.SourcesModel
 @Database(entities = [SourcesModel::class,ArticleModel::class], version = 1, exportSchema = false)
 abstract class LocalDatabase : RoomDatabase() {
     abstract fun getDao(): LocalRepositoryDao
+    
 }
