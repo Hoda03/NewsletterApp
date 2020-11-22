313c11cf4d673d18bcbee18f24dd6d22615a69f2
chaymae4 <71353669+chaymae4@users.noreply.github.com>
2020-11-22 14:29:01 +0100

    Update NewsSourcesAdapterClickEvents.kt

--- NewsletterAndroidApp/app/src/main/java/com/newsletter/app/ui/interfaces/NewsSourcesAdapterClickEvents.kt
+++ NewsletterAndroidApp/app/src/main/java/com/newsletter/app/ui/interfaces/NewsSourcesAdapterClickEvents.kt
@@ -3,5 +3,7 @@ package com.newsletter.app.ui.interfaces
 import com.newsletter.app.dataModels.SourcesModel
 
 interface NewsSourcesAdapterClickEvents {
+    
     fun onItemViewClick(model: SourcesModel)
+    
 }
