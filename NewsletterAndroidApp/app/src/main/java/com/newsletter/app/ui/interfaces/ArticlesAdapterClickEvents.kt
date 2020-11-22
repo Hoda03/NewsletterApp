98dd227fd93dc556fd46f578f305a3e8b501284b
chaymae4 <71353669+chaymae4@users.noreply.github.com>
2020-11-22 14:28:23 +0100

    Update ArticlesAdapterClickEvents.kt

--- NewsletterAndroidApp/app/src/main/java/com/newsletter/app/ui/interfaces/ArticlesAdapterClickEvents.kt
+++ NewsletterAndroidApp/app/src/main/java/com/newsletter/app/ui/interfaces/ArticlesAdapterClickEvents.kt
@@ -3,6 +3,8 @@ package com.newsletter.app.ui.interfaces
 import com.newsletter.app.dataModels.ArticleModel
 
 interface ArticlesAdapterClickEvents {
+    
     fun onItemViewClick(model: ArticleModel)
     fun onFavoriteClick(model: ArticleModel)
+    
 }
